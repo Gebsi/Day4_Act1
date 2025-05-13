a1c9211d3e04865d5ead474fd57adf0c9598530e
honeylyn2002 <93508285+Gebsi@users.noreply.github.com>
2025-05-13 17:56:06 +0800

    Add files via upload

---
+++ Tahud_day4_act1.py
@@ -0,0 +1,6 @@
+a, b = 10, 5
+x, y = True, False
+
+print(f"Add; {a+b}, Sub: {a-b}, Mul: {a*b}, Div: {a/b}, Mod: {a%b}, Exp: {a**b}, Floor Div: {a//b}")
+print(f"AND: {x and y}, OR: {x or y}, NOT: {not x}")
+print(f"Eq: {a==b}, Neq: {a!=b}, GT: {a>b}, LT:{a<b}, GTE: {a>=b}, LTE: {a<=b},")
